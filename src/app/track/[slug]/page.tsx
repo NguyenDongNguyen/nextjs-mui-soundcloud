fe19e44d3523854e4e8219182f54b5ce9ba7d093
NguyenDongNguyen <ndn030802@gmail.com>
2024-03-24 18:50:05 +0700

    design UI, route, dynamic route, fetch data

---
+++ src/app/track/[slug]/page.tsx
@@ -0,0 +1,7 @@
+const DetailTrackPage = (props: any) => {
+    const { params } = props;
+    console.log('>>> check params: ', params);
+    return <div>DetailTrackPage</div>;
+};
+
+export default DetailTrackPage;
